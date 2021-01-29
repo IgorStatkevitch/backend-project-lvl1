664b05bcd46912a67d7f99e2d7a01fedd28fc0eb
Igor Statkevich <igorblood906@gmail.com>
2021-01-29 16:34:05 +0300

    Add brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import * as index from '../src/index.js';
+
+const brainCalc = () => {
+  const a = index.getRandomNum();
+  const b = index.getRandomNum();
+  const operators = ['+', '-', '*'];
+  const opIndex = Math.floor(Math.random() * operators.length);
+  const operator = operators[opIndex];
+
+  const question = `What is the result of the expression?\nQuestion: ${a} ${operator} ${b}\nYour Answer: `;
+  const userAnswer = readlineSync.question(question);
+  let answer = 0;
+  switch (opIndex) {
+    case 0:
+      answer = a + b;
+      break;
+    case 1:
+      answer = a - b;
+      break;
+    case 2:
+      answer = a * b;
+      break;
+    default:
+  }
+
+  return {
+    answer,
+    userAnswer,
+    roundScore: +userAnswer === answer ? 1 : 0,
+  };
+};
+index.startGame(brainCalc);
