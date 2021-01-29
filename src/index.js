00a68694bd5adbd581690ead63cd02cfc690d187
Igor Statkevich <igorblood906@gmail.com>
2021-01-29 16:32:38 +0300

    Add index.js for general logic

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import '../bin/brain-calc.js';
+
+const name = readlineSync.question(
+  'Welcome to the Brain Games!\nMay I have your name? ',
+);
+console.log(`Hello, ${name}!`);
+
+const getRandomNum = () => Math.floor(Math.random() * 50);
+
+const wrongAnswer = (userAnswer, answer) => {
+  console.log(
+    `'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.`,
+  );
+  console.log(`Let's try again, ${name}`);
+};
+
+const startGame = (game) => {
+  let score = 0;
+  while (score < 3) {
+    const { answer, userAnswer, roundScore } = game();
+
+    if (!roundScore) {
+      wrongAnswer(userAnswer, answer);
+      break;
+    } else {
+      console.log('Correct!');
+      score += 1;
+    }
+    console.log(`Congratulations, ${name}`);
+  }
+};
+export { startGame, getRandomNum, wrongAnswer };
