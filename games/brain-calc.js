5804a3cfdfc5e54b8f6b474cfe3456e7049df10d
Igor Statkevich <igorblood906@gmail.com>
2021-02-01 10:54:53 +0300

    Fix code length

--- games/brain-calc.js
+++ games/brain-calc.js
@@ -8,7 +8,6 @@ const brainCalc = () => {
   const operators = ['+', '-', '*'];
   const opIndex = Math.floor(Math.random() * operators.length);
   const operator = operators[opIndex];
-
   const question = `What is the result of the expression?\nQuestion: ${a} ${operator} ${b}\nYour Answer: `;
   const userAnswer = readlineSync.question(question);
   let answer = 0;
