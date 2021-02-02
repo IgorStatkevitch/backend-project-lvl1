41279ee6b3c6753182e198c8ccac8f758e0f4814
Igor Statkevich <igorblood906@gmail.com>
2021-02-02 15:21:01 +0300

    Add brain-prime game

---
+++ games/brain-prime.js
@@ -0,0 +1,26 @@
+import readlineSync from 'readline-sync';
+import startGame from '../src/index.js';
+import getRandomNum from '../src/utils.js';
+
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+const brainPrime = () => {
+  const x = getRandomNum(0, 100);
+  const userAnswer = readlineSync.question(`Question: ${x}\nYour answer: `);
+  const isPrime = (num) => {
+    for (let i = 2; i < num; i += 1) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return num > 1;
+  };
+  const answer = isPrime(x) === true ? 'yes' : 'no';
+
+  return {
+    answer,
+    userAnswer,
+    roundScore: userAnswer.toLowerCase() === answer.toLowerCase() ? 1 : 0,
+  };
+};
+export default () => startGame(brainPrime);
