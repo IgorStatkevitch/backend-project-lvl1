ff809faf98e46a04e7300ee96478c99f8e4478aa
Igor Statkevich <igorblood906@gmail.com>
2021-02-02 15:54:47 +0300

    Clean the code

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -4,15 +4,17 @@ import getRandomNum from '../src/utils.js';
 
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
 const brainPrime = () => {
   const x = getRandomNum(0, 100);
   const userAnswer = readlineSync.question(`Question: ${x}\nYour answer: `);
-  const isPrime = (num) => {
-    for (let i = 2; i < num; i += 1) {
-      if (num % i === 0) return false;
-    }
-    return num > 1;
-  };
+
   const answer = isPrime(x) ? 'yes' : 'no';
 
   return {
